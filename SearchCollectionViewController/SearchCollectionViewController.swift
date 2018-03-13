ef12c76f21148c2534f2d787db0cc69f8b64e770
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2018-03-13 16:14:34 +0700

    Added collectionview

---
+++ SearchCollectionViewController/SearchCollectionViewController.swift
@@ -0,0 +1,89 @@
+//
+//  SearchCollectionViewController.swift
+//  Search
+//
+//  Created by Anh Tuan Nguyen on 3/13/18.
+//  Copyright © 2018 com. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class SearchCollectionViewController: UICollectionViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: UICollectionViewDataSource
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {        
+        case UICollectionElementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                             withReuseIdentifier: "Header",
+                                                                             for: indexPath) as! SearchHeaderView
+            if indexPath.section == 0 {
+                headerView.headerLabel.text = "     CATEGORY"
+            } else if indexPath.section == 1 {
+                headerView.headerLabel.text = "     DATE"
+            }
+            return headerView
+        default:
+            assert(false, "Unexpected element kind")
+        }
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        if indexPath.section == 1 {
+            cell.frame.size = CGSize(width: 414, height: 50)
+            
+        }
+        
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ SearchCollectionViewController/SearchHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  SearchHeaderView.swift
+//  Search
+//
+//  Created by Anh Tuan Nguyen on 3/13/18.
+//  Copyright © 2018 com. All rights reserved.
+//
+
+import UIKit
+
+class SearchHeaderView: UICollectionReusableView {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    
+}
